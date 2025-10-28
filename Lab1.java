ac0a6c7433653846e9d478f6938432bddf0b428d
SadiaAminZuberi <sadiaaminzuberi@gmail.com>
2025-10-28 18:17:21 +0500

    Add files via upload
    Initial commit

---
+++ Lab1.java
@@ -0,0 +1,9 @@
+package mylab1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        int number = 5;
+        int square = number * number;
+        System.out.println("The square of " + number + " is " + square);
+    }
+}
